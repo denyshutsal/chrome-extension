82a82644696e84f6ff90e41b648035c778dde9c4
Denys Hutsal <denyshutsal@gmail.com>
2023-01-11 17:25:10 +0100

    feat: implement saving tabs

--- main.js
+++ main.js
@@ -3,12 +3,21 @@ const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
 const deleteBtn = document.getElementById("delete-btn")
+const tabBtn = document.getElementById("tab-btn")
 const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
 
 if (leadsFromLocalStorage) {
   myLeads = leadsFromLocalStorage
   render(myLeads)
 }
+
+tabBtn.addEventListener("click", function(){    
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+      myLeads.push(tabs[0].url)
+      localStorage.setItem("myLeads", JSON.stringify(myLeads))
+      render(myLeads)
+  })
+})
 
 function render(leads) {
   let listItems = ""
